f554dee61425e3466e10f113b7215f75fe021ba6
xavier <ulgenmf@gmail.com>
2023-03-25 16:54:47 +0100

    c

--- src/constants/images.js
+++ src/constants/images.js
@@ -6,7 +6,7 @@ import css from "../assets/css.png";
 import me1 from "../assets/me/me1.jpeg";
 import me2 from "../assets/me/me2.jpeg";
 import me3 from "../assets/me/me3.jpeg";
-import me4 from "../assets/me/a.png";
+
 import figma from "../assets/figma.png";
 import flutter from "../assets/flutter.png";
 import git from "../assets/git.png";
@@ -96,7 +96,7 @@ export default {
 	me1,
 	me2,
 	me3,
-	me4,
+
 	trash,
 	start,
 	thisPc,
